c2d152e148f36e7123c69bd449d007fa3dfec794
ruslanepi <ruslanep2@gmail.com>
2022-09-02 17:49:11 +0300

    added useContext custom hook

---
+++ 12-sidebar-modal/src/Home.js
@@ -0,0 +1,18 @@
+import React, {} from 'react'
+import {FaBars} from 'react-icons/fa'
+import {  useGlobalContext } from './context'
+
+const Home = () => {
+  const data = useGlobalContext()
+  console.log(data);
+  return (
+    <main>
+      <button className='sidebar-toggle'>
+        <FaBars />
+      </button>
+      <button className='btn'> show modal</button>
+    </main>
+  )
+}
+
+export default Home
