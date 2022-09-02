bda7437e7308a494ecdd5428dab29f6e3cfb0191
ruslanepi <ruslanep2@gmail.com>
2022-09-02 22:08:08 +0300

    added sidebar and modal opening with useGlobalContext

--- 12-sidebar-modal/src/Modal.js
+++ 12-sidebar-modal/src/Modal.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import {FaTimes} from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from './context'
 
 const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext()
+  console.log(isModalOpen)
   return (
-    <div className={`modal-overlay `}>
-      <div className="modal-container">
+    <div
+      className={`${
+        isModalOpen ? 'show-modal modal-overlay' : 'modal-overlay'
+      }`}
+    >
+      <div className='modal-container'>
         <h3>modal content</h3>
-        <button className="close-modal-btn"><FaTimes/></button>
+        <button className='close-modal-btn' onClick={closeModal}>
+          <FaTimes />
+        </button>
       </div>
     </div>
   )
